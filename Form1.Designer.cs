e67341ba8fd522c44d9dfc5168d86f9b0e098a67
ecropr <ec_ropr@mail.ru>
2016-06-16 13:05:26 +0700

    Изменено имя в шапке

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -198,7 +198,7 @@
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "East 2016";
+            this.Text = "East 2016 v2.0";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
