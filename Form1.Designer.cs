2866fcc6a3335fc6529d22b3ecdd3358317f45bd
buravil <buravil@mail.ru>
2016-12-07 15:40:29 +0700

    Исправлено

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -576,7 +576,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "East v2.5.18";
+            this.Text = "East v2.5.19";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
